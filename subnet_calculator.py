ad5cdb84f225138692fe7689c2747f7f9a982b5c
cryptohypocrisy <36382502+cryptohypocrisy@users.noreply.github.com>
2018-05-11 02:08:58 -0400

    update subnet_calculator.py
    Added ip_check() and calculate_subnet_info(), functional for network and broadcast addresses

--- subnet_calculator.py
+++ subnet_calculator.py
@@ -9,25 +9,12 @@ def get_ip():
     while True:
         ip = input("\nEnter IP in dotted decimal notation: ")
         ip = ip.strip()
-        ip_list = []
+        ip_list = ip.split(".")
 
-        if len(ip.split(".")) != 4:
-            print("Invalid IP address, try again.")
-            continue
-
-        for n in ip.split("."):
-            if n.isdigit() and int(n) <= 255 and int(n) >= 0:
-                ip_list.append(n)
-
-        if len(ip_list) != 4:
-            print("Invalid IP address, try again.")
-            continue
-        else:
+        if len(ip_list) == 4 and check_ip(ip_list):
             return ip_list
-
-
-def check_ip():
-    pass
+        else:
+            print("Invalid IP address, try again.")
 
 
 def get_mask():
@@ -36,10 +23,22 @@ def get_mask():
         mask = mask.strip()
         mask_list = mask.split(".")
 
-        if check_mask(mask_list) and len(mask_list) == 4:
+        if len(mask_list) == 4 and check_mask(mask_list):
             return mask_list
         else:
             print("Invalid subnet mask, try again. ")
+
+
+def check_ip(ip_list):
+    try:
+        for n in ip_list:
+            n = int(n)
+            if n < 0 or n > 255:
+                return False
+    except ValueError:
+        return False
+
+    return True
 
 
 def check_mask(mask_list):
@@ -50,14 +49,31 @@ def check_mask(mask_list):
         for n in mask_list:
             n = int(n)
             if n > a or n not in valid_masks or a < 255 and n != 0:
-                print("1")
                 return False
             a = n
     except ValueError:
-        print('4')
         return False
 
     return True
+
+
+def calculate_subnet_info(ip, mask):
+    net_address_list = []
+    broad_address_list = []
+    wildcard_list = []
+
+    for n in mask:
+        x = 255 - int(n)
+        wildcard_list.append(x)
+
+    for i in range(4):
+        net_address = int(ip[i]) & int(mask[i])
+        net_address_list.append(str(net_address))
+        broad_address = int(ip[i]) | int(wildcard_list[i])
+        broad_address_list.append(str(broad_address))
+
+    print("{:20} {:10}".format("Network Address:", ".".join(net_address_list)))
+    print("{:20} {:10}".format("Broadcast Address:", ".".join(broad_address_list)))
 
 
 def main():
@@ -66,7 +82,7 @@ def main():
     while True:
         ip_list = get_ip()
         mask_list = get_mask()
-        print(ip_list, mask_list, sep="\n")
+        calculate_subnet_info(ip_list, mask_list)
 
 
 if __name__ == "__main__":
